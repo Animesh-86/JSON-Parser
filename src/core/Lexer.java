42cd19852a4cc150fa6b7544dce61a831bc66c5b
Animesh <animeshiit0806@gmail.com>
2025-09-10 23:02:56 +0530

    Completed Core Functionality

--- src/core/Lexer.java
+++ src/core/Lexer.java
@@ -1,10 +1,10 @@
 package core;
 
-public class Tokenizer {
+public class Lexer {
     private String input;
     private int pos;
 
-    public Tokenizer(String input, int pos) {
+    public Lexer(String input, int pos) {
         this.input = input;
         this.pos = pos;
     }
@@ -32,23 +32,50 @@ public class Tokenizer {
             case '\0': return new Token(TokenType.EOF, null);
             default:
                 if(Character.isDigit(c) || c == '-') return numberToken();
-                if(c == 't' || c == 'f') return boolenToken();
+                if(c == 't' || c == 'f') return booleanToken();
                 if(c == 'n') return nullToken();
                 throw new RuntimeException("Unexpected chraracter: " + c);
         }
     }
 
     private Token nullToken() {
+        if(input.startsWith("null", pos)){
+            pos += 4;
+            return new Token(TokenType.NULL, null);
+        }
+        throw new RuntimeException("Invalid null");
     }
 
-    private Token boolenToken() {
+    private Token booleanToken() {
+        if(input.startsWith("true", pos)){
+            pos += 4;
+            return new Token(TokenType.BOOLEAN, "true");
+        }
+
+        if(input.startsWith("false", pos)) {
+            pos += 5;
+            return new Token(TokenType.BOOLEAN, "false");
+        }
+        throw new RuntimeException("Invalid boolean");
     }
 
     private Token numberToken() {
+        StringBuilder sb = new StringBuilder();
+        while(Character.isDigit(currentChar()) || currentChar() == '.' || currentChar() == '-'){
+            sb.append(currentChar());
+            pos++;
+        }
+        return new Token(TokenType.NUMBER, sb.toString());
     }
 
     private Token stringToken() {
-        
+        pos++;
+        StringBuilder sb = new StringBuilder();
+        while(currentChar() != '"'){
+            sb.append(currentChar());
+            pos++;
+        }
+        pos++;
+        return new Token(TokenType.STRING, sb.toString());
     }
-
 }
---
+++ src/core/Parser.java
@@ -0,0 +1,94 @@
+package core;
+
+public class Parser {
+    private Lexer lexer;
+    private Token currentToken;
+
+    Parser(Lexer lexer) {
+        this.lexer = lexer;
+        this.currentToken = lexer.nextToken();
+    }
+
+    private void eat(TokenType type) {
+        if (currentToken.type == type) {
+            currentToken = lexer.nextToken();
+        } else {
+            throw new RuntimeException("Expected " + type + " but found " + currentToken.type);
+        }
+    }
+
+    public JsonValue parse() {
+        return parseValue();
+    }
+
+    private JsonValue parseValue() {
+        switch (currentToken.type) {
+            case BEGIN_OBJECT:
+                return parseObject();
+            case BEGIN_ARRAY:
+                return parseArray();
+            case STRING:
+                String s = currentToken.value;
+                eat(TokenType.STRING);
+                return new JsonString(s);
+            case NUMBER:
+                double n = Double.parseDouble(currentToken.value);
+                eat(TokenType.NUMBER);
+                return new JsonNumber((long) n);
+            case BOOLEAN:
+                boolean b = Boolean.parseBoolean(currentToken.value);
+                eat(TokenType.BOOLEAN);
+                return new JsonBoolean(b);
+            case NULL:
+                eat(TokenType.NULL);
+                return new JsonNull();
+            case EOF:
+                return null;
+            default:
+                throw new RuntimeException("Unexpected token: " + currentToken);
+        }
+    }
+
+    private JsonObject parseObject() {
+        eat(TokenType.BEGIN_OBJECT);
+        JsonObject obj = new JsonObject();
+
+        if (currentToken.type != TokenType.END_OBJECT) {
+            do {
+                String key = currentToken.value;
+                eat(TokenType.STRING);
+                eat(TokenType.COLON);
+                JsonValue value = parseValue();
+                obj.map.put(key, value);
+
+                if (currentToken.type == TokenType.COMMA) {
+                    eat(TokenType.COMMA);
+                } else {
+                    break;
+                }
+            } while (true);
+        }
+
+        eat(TokenType.END_OBJECT);
+        return obj;
+    }
+
+    private JsonArray parseArray() {
+        eat(TokenType.BEGIN_ARRAY);
+        JsonArray arr = new JsonArray();
+
+        if (currentToken.type != TokenType.END_ARRAY) {
+            do {
+                arr.list.add(parseValue());
+                if (currentToken.type == TokenType.COMMA) {
+                    eat(TokenType.COMMA);
+                } else {
+                    break;
+                }
+            } while (true);
+        }
+
+        eat(TokenType.END_ARRAY);
+        return arr;
+    }
+}
