034499ea140ce19d8cfc93f5df6845e4981060d6
Animesh <animeshiit0806@gmail.com>
2025-09-11 21:05:19 +0530

    Starting Phase 2 Error Handling

--- src/core/Token.java
+++ src/core/Token.java
@@ -3,10 +3,14 @@ package core;
 public class Token {
     TokenType type;
     String value;
+    public int line;
+    public int column;
 
     public Token(TokenType type, String value) {
         this.type = type;
         this.value = value;
+        this.line = line;
+        this.column = column;
     }
 
     @Override
